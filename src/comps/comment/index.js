ee1cd1a281049960c9a39615d42440f4b161923d
Moonwalker <abhisk.mhra@gmail.com>
2021-05-22 22:08:46 +0530

    comments input method update to firestore

--- src/comps/comment/index.js
+++ src/comps/comment/index.js
@@ -1,11 +1,11 @@
-export default function Comment({ username, caption }) {
+export default function Comment({ username, comment }) {
   return (
     <div class="comment">
       <p>
-        <span style={{ fontWeight: "500", marginRight: "4px" }}>
+        <span style={{ color: "black", fontWeight: "500", marginRight: "4px" }}>
           {username}
         </span>
-        {caption}
+        {comment}
       </p>
     </div>
   );
--- src/comps/commentInput/index.js
+++ src/comps/commentInput/index.js
@@ -1,18 +1,26 @@
 import "./styles.css";
 import { useState } from "react";
+import { firestore } from "../../services/firebaseConfig";
 const CommentInput = ({ comments, id }) => {
   const [comment, setComment] = useState("");
 
+  //   This function adds new comments to the comment array from firebase and updates the data on firebase simultaneously
   const addComment = (e) => {
     e.preventDefault();
-    let newCommentArray = comments.push(comment);
-    console.log(newCommentArray);
+    comments.push(comment);
+
+    // Updating to firestore's doc
+    firestore.collection("posts").doc(id).update({
+      comments: comments,
+    });
     setComment("");
   };
 
   return (
-    <form id="comment_input" onSubmit={addComment}>
+    <form onSubmit={addComment}>
       <input
+        value={comment}
+        className="comment_input"
         onChange={(e) => setComment(e.target.value)}
         placeholder="Leave a comment ..."
         size="xs"
