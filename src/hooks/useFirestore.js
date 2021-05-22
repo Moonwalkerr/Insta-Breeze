6ca1fae01985c5f59aee59cc65d0efa63243ca7d
Moonwalker <abhisk.mhra@gmail.com>
2021-05-22 15:50:38 +0530

    useFirestore hook

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { fireStore } from "../firebaseConfig";
+
+const useFirestore = (collectionName) => {
+  const [doc, setDoc] = useState([]);
+  useEffect(() => {
+    const unSubscribe = fireStore
+      .collection(collectionName)
+      // ordering our data  in date order in descending order
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snapshot) => {
+        let documents = [];
+
+        snapshot.forEach((doc) => {
+          documents.push({ ...doc.data(), id: doc.id });
+        });
+        setDoc(documents);
+      });
+    return () => unSubscribe(); // cleanup function
+  }, [collectionName]);
+  // console.log(doc);
+  return { doc };
+};
+
+export default useFirestore;
