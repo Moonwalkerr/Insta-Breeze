1c4a90da89e5494a046b791c9d6199a12cfa8929
Moonwalker <abhisk.mhra@gmail.com>
2021-05-22 20:29:30 +0530

    removed unwanted methods from context layer

--- src/context/context.js
+++ src/context/context.js
@@ -1,13 +1,10 @@
-import { useEffect, useState, createContext } from "react";
-import auth, { firestore } from "../services/firebaseConfig";
+import auth from "../services/firebaseConfig";
 
 export const AppContext = createContext();
 
 const AppContextProvder = (props) => {
   // User's piece of state
   const [user, setUser] = useState(null);
-  const [posts, setPosts] = useState([]);
-  // const [comments, setComments] = useState([]);
 
   // Checking if user exists, whenever we reload the tab
   auth.onAuthStateChanged((firebaseUser) => {
@@ -15,31 +12,11 @@ const AppContextProvder = (props) => {
       setUser(firebaseUser);
     }
   });
-  const [loading, setLoading] = useState(false);
-
-  // useEffect(() => {
-  //   let document = [];
-  //   setLoading(true);
-  //   const unSubscribe = firestore
-  //     .collection("posts")
-  //     .orderBy("timestamp", "desc")
-  //     .onSnapshot((snapshot) => {
-  //       snapshot.forEach((docs) => {
-  //         console.log(docs.data().caption);
-  //         setLoading(false);
-  //         document.push({ post: docs.data(), id: docs.id });
-  //       });
-  //       setPosts(document);
-  //     });
-  //   return () => unSubscribe(); // cleanup function
-  // }, []);
   return (
     <AppContext.Provider
       // These values can be used whenever and wherver the component lies if AuthContext Provider has the parent consumer within itself
       value={{
         user: [user, setUser],
-        posts: [posts, setPosts],
-        loading: [loading, setLoading],
       }}
     >
       {props.children}
