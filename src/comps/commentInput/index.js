c647d2c2dd0c01470e36374d5ffad0ff20a3b156
Moonwalker <abhisk.mhra@gmail.com>
2021-05-22 21:44:58 +0530

    adding styles to comment input section

--- src/comps/commentInput/index.js
+++ src/comps/commentInput/index.js
@@ -1,16 +1,24 @@
-import { Input } from "@chakra-ui/input";
-
+import "./styles.css";
+import { useState } from "react";
 const CommentInput = ({ comments, id }) => {
+  const [comment, setComment] = useState("");
+
+  const addComment = (e) => {
+    e.preventDefault();
+    let newCommentArray = comments.push(comment);
+    console.log(newCommentArray);
+    setComment("");
+  };
+
   return (
-    <Input
-      border="1px solid lightgrey"
-      _focus={{
-        border: "none",
-        outline: "none",
-      }}
-      placeholder="Leave a comment ..."
-      size="xs"
-    />
+    <form id="comment_input" onSubmit={addComment}>
+      <input
+        onChange={(e) => setComment(e.target.value)}
+        placeholder="Leave a comment ..."
+        size="xs"
+      />
+      <button type="submit" display="none" />
+    </form>
   );
 };
 
