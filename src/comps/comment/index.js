da8743ce9f799425a0dff1f3990957f991eabf86
Moonwalker <abhisk.mhra@gmail.com>
2021-05-22 22:20:20 +0530

    fix comments input taking blank values

--- src/comps/comment/index.js
+++ src/comps/comment/index.js
@@ -1,12 +1,22 @@
+import { Flex, Text } from "@chakra-ui/layout";
+
 export default function Comment({ username, comment }) {
   return (
-    <div class="comment">
-      <p>
-        <span style={{ color: "black", fontWeight: "500", marginRight: "4px" }}>
+    <Flex ml="4" fontSize="12px" width="100%" alignItems="center">
+      <Text size="md" color="#333" fontFamily="Merriweather, sans-serif">
+        <span
+          style={{
+            fontSize: "14px",
+            letterSpacing: "0px",
+            color: "purple",
+            fontWeight: "700",
+            marginRight: "4px",
+          }}
+        >
           {username}
         </span>
         {comment}
-      </p>
-    </div>
+      </Text>
+    </Flex>
   );
 }
--- src/comps/commentInput/index.js
+++ src/comps/commentInput/index.js
@@ -1,19 +1,25 @@
 import "./styles.css";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { AppContext } from "../../context/context";
 import { firestore } from "../../services/firebaseConfig";
 const CommentInput = ({ comments, id }) => {
   const [comment, setComment] = useState("");
+  const user = useContext(AppContext).user[0];
 
   //   This function adds new comments to the comment array from firebase and updates the data on firebase simultaneously
   const addComment = (e) => {
     e.preventDefault();
-    comments.push(comment);
+    if (comment !== "") {
+      comments.push({ userName: user.displayName, comment: comment });
 
-    // Updating to firestore's doc
-    firestore.collection("posts").doc(id).update({
-      comments: comments,
-    });
-    setComment("");
+      // Updating to firestore's doc
+      firestore.collection("posts").doc(id).update({
+        comments: comments,
+      });
+      setComment("");
+    } else {
+      alert("Please Enter a comment !");
+    }
   };
 
   return (
