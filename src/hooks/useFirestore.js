488e6771ca9dd54279c8cc09615d248295fedce0
Moonwalker <abhisk.mhra@gmail.com>
2021-05-22 16:23:28 +0530

    useFirestore hook checkpoint : working !

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,25 +1,30 @@
 import { useState, useEffect } from "react";
-import { fireStore } from "../firebaseConfig";
+import { firestore } from "../services/firebaseConfig";
 
 const useFirestore = (collectionName) => {
   const [doc, setDoc] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    const unSubscribe = fireStore
-      .collection(collectionName)
+    const unSubscribe = firestore
+      .collection("posts")
       // ordering our data  in date order in descending order
-      .orderBy("createdAt", "desc")
+      .orderBy("timestamp", "desc")
       .onSnapshot((snapshot) => {
-        let documents = [];
-
-        snapshot.forEach((doc) => {
-          documents.push({ ...doc.data(), id: doc.id });
-        });
-        setDoc(documents);
+        doc.push(
+          snapshot.forEach((docs) => {
+            if (docs.data().userName) {
+              console.log(docs.data().userName);
+              setLoading(false);
+              setDoc({ ...docs.data() });
+            }
+          })
+        );
       });
+    // setDoc(doc);
     return () => unSubscribe(); // cleanup function
   }, [collectionName]);
   // console.log(doc);
-  return { doc };
+  return { doc, loading };
 };
 
 export default useFirestore;
