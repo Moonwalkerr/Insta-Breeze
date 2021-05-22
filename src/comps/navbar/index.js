ea08ce4b3b085406d2e8cd452da221250eb17266
Moonwalker <abhisk.mhra@gmail.com>
2021-05-22 11:32:09 +0530

    WIP Navbar

---
+++ src/comps/navbar/index.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <span>Insta-Breeze</span>
+        <button>SignIn</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
