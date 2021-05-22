8dc23e884eb81ffd4d3e0f0bd04d063f0c05ed0f
Moonwalker <abhisk.mhra@gmail.com>
2021-05-22 12:59:38 +0530

    imported chakraporvider theme to index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import theme from "./theme/theme";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
