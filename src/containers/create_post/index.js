aebaf26b24601b48ee6f1611c3c38098a6a0b452
Moonwalker <abhisk.mhra@gmail.com>
2021-05-22 14:54:55 +0530

    WIP CreatePost Container

--- src/containers/create_post/index.js
+++ src/containers/create_post/index.js
@@ -1,5 +1,137 @@
-const CreatePost = () => {
-  return <Container></Container>;
+import { useContext, useState } from "react";
+import { SignInBtn } from "../../comps";
+import { AppContext } from "../../context/context";
+import { Button, Textarea } from "@chakra-ui/react";
+import { RiGalleryFill } from "react-icons/ri";
+import { Flex, Spacer } from "@chakra-ui/react";
+import { FaUpload } from "react-icons/fa";
+import { firestore, storage, timestamp } from "../../services/firebaseConfig";
+import generateID from "../../helpers/functions";
+
+const Createpost = () => {
+  const user = useContext(AppContext).user[0];
+
+  const [caption, setCaption] = useState("");
+  const [progress, setProgress] = useState(null);
+  const [image_upload, setImage_upload] = useState(null);
+  const image_prev = document.getElementById("image-prev");
+
+  const handleCaptionChange = (e) => {
+    setCaption(e.target.value);
+  };
+
+  const handle_image_upload = (e) => {
+    if (e.target.files[0]) {
+      image_prev.src = URL.createObjectURL(e.target.files[0]);
+      setImage_upload(e.target.files[0]);
+      image_prev.style.display = "block";
+    }
+  };
+  const uploadPost = async () => {
+    const img_id = generateID(10);
+    const storageRef = storage.ref(`images/${img_id}`);
+    if (image_upload) {
+      await storageRef.put(image_upload).on(
+        "state_changed",
+        (snapshot) => {
+          setProgress(
+            Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+          );
+        },
+        (error) => alert(error.message),
+        async () => {
+          let image_url = await storageRef.getDownloadURL();
+          let data = {
+            userName: user.displayName,
+            photoUrl: user.photoURL,
+            imageUrl: image_url,
+            caption: caption,
+            timestamp: timestamp,
+            likes: 0,
+            comments: [],
+          };
+          firestore.collection("posts").add(data).then(clearPostContainer);
+        }
+      );
+    } else {
+      let data = {
+        userName: user.displayName,
+        photoUrl: user.photoURL,
+        caption: caption,
+        timestamp: timestamp,
+        likes: 0,
+        comments: [],
+      };
+      firestore.collection("posts").add(data).then(clearPostContainer);
+    }
+  };
+
+  function clearPostContainer() {
+    image_prev.src = "";
+    image_prev.style.display = "none";
+    setProgress(null);
+    setCaption("");
+  }
+
+  return (
+    <Flex
+      margin="2rem 0rem"
+      className="create__post"
+      borderRadius="10px"
+      bg="#fafafa"
+      maxWidth="65vw"
+    >
+      {user ? (
+        <Flex
+          padding="10px"
+          flexDirection="column"
+          width="700px"
+          flexWrap="nowrap"
+        >
+          <h4
+            style={{
+              fontFamily: "Merriweather,sans-serif",
+              fontSize: "13px",
+              fontWeight: "600",
+              letterSpacing: "1px",
+            }}
+          >
+            Create a Post
+          </h4>
+          <Spacer />
+          <Textarea
+            value={caption}
+            onChange={handleCaptionChange}
+            border="none"
+            focusBorderColor="#fafafafa"
+            height="22vh"
+            marginBottom="14px"
+            placeholder="Write your caption here..."
+          />
+          <img id="image-prev" alt="img-prev" />
+          <Flex justifyContent="space-between" flexWrap="nowrap" color="purple">
+            <label htmlFor="fileInput">
+              <RiGalleryFill cursor="pointer" fontSize="2rem" />
+            </label>
+            <input type="file" onChange={handle_image_upload} id="fileInput" />
+            <Button
+              isLoading={progress ? true : false}
+              onClick={uploadPost}
+              variant="outline"
+              height="2rem"
+              fontSize="1rem"
+              colorScheme="purple"
+              leftIcon={<FaUpload />}
+            >
+              Post {progress && progress}
+            </Button>
+          </Flex>
+        </Flex>
+      ) : (
+        <SignInBtn />
+      )}
+    </Flex>
+  );
 };
 
-export default CreatePost;
+export default Createpost;
