453bb9f55d825afff1dc25ef053acbc1201c3614
Moonwalker <abhisk.mhra@gmail.com>
2021-05-22 11:53:43 +0530

    navbar wip

--- src/comps/navbar/index.js
+++ src/comps/navbar/index.js
@@ -1,10 +1,26 @@
+import { Flex, Spacer, Text } from "@chakra-ui/react";
+import { SignInBtn } from "..";
+import "./styles.css";
+
 const Navbar = () => {
   return (
-    <header>
-      <nav>
-        <span>Insta-Breeze</span>
-        <button>SignIn</button>
-      </nav>
+    <header styles={{ width: "100vw" }}>
+      <Flex
+        width="97%"
+        flex-direction
+        p={20}
+        // justifyContent="space-around"
+        bg="var(--primary)"
+      >
+        <Text letterSpacing={4}>Insta-Breeze</Text>
+        <Spacer />
+        <Text letterSpacing={4} fontSize={20}>
+          Insta-Breeze
+        </Text>
+        <Spacer />
+        <SignInBtn />
+        {/* </nav> */}
+      </Flex>
     </header>
   );
 };
--- src/comps/signInBtn/index.js
+++ src/comps/signInBtn/index.js
@@ -1,5 +1,19 @@
+import { Button } from "@chakra-ui/button";
+import { FcGoogle } from "react-icons/fc";
+
 const SignInBtn = () => {
-  return <div className="SignInBtn"></div>;
+  return (
+    <Button
+      fontFamily="Merriweather,sans-serif"
+      p={6}
+      cursor="pointer"
+      leftIcon={<FcGoogle />}
+      colorScheme="teal"
+      variant="solid"
+    >
+      Sign In With Google
+    </Button>
+  );
 };
 
 export default SignInBtn;
